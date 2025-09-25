9797d65132ef3289130c7be7f7f3e5a6b03b1c5b
JoseMyPaez <70114189+JoseMyPaez@users.noreply.github.com>
2025-09-25 15:44:43 +0000

    First

---
+++ firstExercise.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int calculateTotal(int hours, int rate){
+    return hours * rate;
+}
+
+void printInvoice(string plate, int hours, int rate) {
+    cout<<"Vehiculo"<<plate<<", Horas: "<<hours<<", Tarifa:"<<rate <<endl;
+    cout<<"Total a pagar: "<<calculateTotal(hours, rate)<<endl;
+}
+
+int main(){
+    string plate;
+    int HoursParking, RatePerHour;
+    cout<<"Dame la placa del vehiculo: ";
+    cin >> plate;
+    cout <<"Dame las horas de estacionamiento: ";
+    cin >> HoursParking;
+    cout <<"Dame la tarifa por hora: ";
+    cin >> RatePerHour;
+    calculateTotal(HoursParking, RatePerHour);
+
+
+}
